3c192a7051a970ad970bbfcf7e97fd8d15a6a72f
shimisunet <xxnanochan@gmail.com>
2019-09-22 15:03:00 +0900

    ブログの記事一覧を出すcomponent追加

---
+++ src/.vuepress/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <article v-for="post in posts" class="article">
+            <a v-bind:href="post.path">{{ post.title }}</a>
+            <p>{{ post.frontmatter.description }}</p>
+        </article>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    computed: {
+        posts() {
+            return this.$site.pages
+                .filter( post => post.path.startsWith('/blog/') )
+                .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.article
+    border 1px solid #333
+</style>
