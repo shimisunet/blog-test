dcf3be7ee01bd68d807087afef9997fe4ca0aec7
shimisunet <xxnanochan@gmail.com>
2019-10-30 23:59:03 +0900

    design update

--- src/.vuepress/components/PostList.vue
+++ src/.vuepress/components/PostList.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>
-        <article v-for="post in posts" class="article">
-            <a v-bind:href="post.path">{{ post.title }}</a>
-            <p>{{ post.frontmatter.description }}</p>
-        </article>
-    </div>
-    
+  <div>
+    <article v-for="post in posts" class="blog-article">
+      <a v-bind:href="post.path">
+        <section class="blog-section">
+          <h2 class="blog-title">
+            {{ post.title }}
+          </h2>
+          <p class="blog-description">
+            {{ post.frontmatter.description }}
+          </p>
+        </section>
+      </a>
+    </article>
+  </div>
 </template>
 
 <script>
@@ -13,14 +20,33 @@ export default {
     computed: {
         posts() {
             return this.$site.pages
-                .filter( post => post.path.startsWith('/blog/') )
-                .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+            .filter( post => post.path.startsWith('/blog/') )
+            .sort( ( a, b ) => new Date( b.frontmatter.date ) - new Date( a.frontmatter.date ) );
+            }
         }
     }
-}
 </script>
 
 <style lang="stylus" scoped>
-.article
-    border 1px solid #333
+.blog-article
+  border-bottom 1px solid #eaecef
+  
+  &:first-child
+    border-top 1px solid #eaecef
+  
+  &:last-child
+    border-bottom none
+
+.blog-section
+  &:hover
+    opacity 0.5
+
+.blog-title
+  border-bottom none
+  margin 18px 0
+  color navy 
+
+.blog-description
+  color #333
+  
 </style>
