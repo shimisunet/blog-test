1da3df1dc07f3c3be67a88e8ffccfc992342d366
shimisunet <xxnanochan@gmail.com>
2019-11-04 18:11:24 +0900

    descriptionとdateのレイアウト変更

--- src/.vuepress/components/PostList.vue
+++ src/.vuepress/components/PostList.vue
@@ -6,10 +6,15 @@
           <h2 class="blog-title">
             {{ post.title }}
           </h2>
-          <p class="blog-description">
-            {{ post.frontmatter.date }}
+          <div class="blog-detail-wrapper">
+            <p class="blog-description">
             {{ post.frontmatter.description }}
           </p>
+          <p>
+            {{ post.frontmatter.date }}
+          </p>
+          </div>
+
         </section>
       </a>
     </article>
@@ -47,7 +52,9 @@ export default {
   margin 18px 0
   color navy 
 
-.blog-description
+.blog-detail-wrapper
+  display flex
   color #333
-  
+  justify-content space-between
+
 </style>
